60feba721e6f21721fc4e596521b96b339ae73e3
Alex Johnson <alex@gritdesign.co>
2017-11-09 19:26:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,16 +1,3 @@
 function happyHolidays() {
 return "Happy holidays!"
 }
-
-
-function happyHolidaysTo() {
- return "Happy holidays, " , ${name} , "!";
-}
-
-function happyHolidayTo() {
-
-}
-
-function holidayCountdown() {
-
-}
